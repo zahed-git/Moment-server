34ef3faa7dc004aec3ab0878220f60d1404b76b2
zahed-git <mdzahed18@gmail.com>
2024-06-24 13:57:28 +0600

    updated login

--- index.js
+++ index.js
@@ -42,10 +42,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const momentUsers = client.db("moment").collection("users")
-    const momentPremiumMembers = client.db("moment").collection("premium-member")
+    // const momentPremiumMembers = client.db("moment").collection("premium-member")
     const momentsucess_story = client.db("moment").collection("sucess_story")
     const momentBio_Data = client.db("moment").collection("biodata")
     const momentContact_req = client.db("moment").collection("contact_req")
@@ -56,7 +56,7 @@ async function run() {
     // -------------------------------------------JWT
     app.post('/jwt', async (req, res) => {
       const user = req.body
-      const token = jwt.sign(user, process.env.ACESS_TOKEN, { expiresIn: '1h' })
+      const token = jwt.sign(user, process.env.ACESS_TOKEN, { expiresIn: '2h' })
       res.send({ token })
     })
 
@@ -64,12 +64,14 @@ async function run() {
     // --------------------------------------------middleware for jwt--user
     const verifyToken = (req, res, next) => {
       const header = req.headers.authorization
+      
       if (!header) {
         return res.status(401).send({ message: 'forbidder access' })
       }
       const token = header.split(' ')[1]
       jwt.verify(token, process.env.ACESS_TOKEN, (err, decoded) => {
         if (err) {
+          console.log('74 hit in veryfy token 2nd',err)
           return res.status(401).send({ message: 'forbidder access' })
 
         }
@@ -81,27 +83,32 @@ async function run() {
     // --use verify admin after verify the token 
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email
+      console.log("86 veryfy admin",email)
       const query = { email: email }
+      console.log(query)
       const user = await momentUsers.findOne(query)
+      console.log("verify admin 90",user)
       const isAdmin = user?.role === 'admin'
       if (!isAdmin) {
         return res.status(401).send({ message: 'forbidden access' })
       }
       next()
     }
-    // const verifyPreium = async (req, res, next) => {
-    //   const email = req.decoded.email
-    //   const query = { email: email }
-    //   const user = await momentUsers.findOne(query)
-    //   const isPremium = user?.usertype === 'premium'
-    //   if (!isPremium) {
-    //     return res.status(401).send({ message: 'forbidden access' })
-    //   }
-    //   next()
-    // }
+    const verifyPreium = async (req, res, next) => {
+      // const email = req.decoded.email
+      console.log(req.decoded)
+      // const query = { email: email }
+      // const user = await momentUsers.findOne(query)
+      // const isPremium = user?.usertype === 'premium'
+      // if (!isPremium) {
+      //   return res.status(401).send({ message: 'forbidden access' })
+      // }
+      next()
+    }
 
     // --------------------------------------------------------------------------users
     app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+      console.log('inpatch /users/admin/:id 107')
       const userId = req.params.id;
       const query = { _id: new ObjectId(userId) }
       const updateDoc = {
@@ -113,6 +120,7 @@ async function run() {
       res.send(result)
     })
     app.patch('/users/premium/:id', verifyToken, verifyAdmin, async (req, res) => {
+      console.log('inpatch /users/premium/:id 119')
       const userId = req.params.id;
       const query = { _id: new ObjectId(userId) }
       const updateDoc = {
@@ -125,6 +133,7 @@ async function run() {
     })
 
     app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+      console.log('dele /users:id 132')
       const userId = req.params.id;
       const query = { _id: new ObjectId(userId) }
       const result = await momentUsers.deleteOne(query)
@@ -132,20 +141,25 @@ async function run() {
 
     })
 
-    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+    app.get('/users', verifyToken,verifyAdmin,  async (req, res) => {
       const result = await momentUsers.find().toArray()
       res.send(result)
     })
-    app.get('/users/:email', async (req, res) => {
+    app.get('/users/:email',verifyToken, async (req, res) => {
+      
       const email = req.params.email
+      console.log("151 get/users/:email",email)
       const query = { email: email }
       const result = await momentUsers.find(query).toArray()
       res.send(result)
     })
 
-    app.get('/users/admin/:email', verifyToken, verifyAdmin, async (req, res) => {
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      console.log('inget /users152')
       const email = req.params.email;
+      console.log("156/users/admin",email)
       const emaiil = req.decoded.email;
+      console.log("158/users/admin",emaiil)
       query = { email: email }
       const user = await momentUsers.findOne(query)
       let admin = false
@@ -204,11 +218,12 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/contact-req', async (req, res) => {
+    app.get('/contact-req',verifyToken, async (req, res) => {
       const result = await momentContact_req.find().toArray()
       res.send(result)
     })
     app.patch('/contact-req/pending/:id', verifyToken, verifyAdmin, async (req, res) => {
+      console.log('inget /users 191')
       const reqId = req.params.id;
       const query = { _id: new ObjectId(reqId) }
       const updateDoc = {
@@ -235,20 +250,24 @@ async function run() {
     })
     // --------------------biodatas---------------------------a
     app.get('/states',async(req, res)=>{
-      // const result=await momentBio_Data.aggregate([
-      //   {
-      //     $group:{
-      //       _id:null,
-      //       Totalbiodata:{
-      //         biodataId
-      //       }
-      //     }
-      //   }
-      // ])
+      const money=await momentContact_req.aggregate([
+        {
+          $group:{
+            _id:null,
+            totalRevenue:{
+              $sum:'$price'
+            }
+          }
+        }
+      ]).toArray()
+      const revenue= money.length>0 ? money[0].totalRevenue:0
       const result=await momentBio_Data.estimatedDocumentCount()
-      res.send({result})
+      const marred= await momentsucess_story.estimatedDocumentCount()
+      
+      res.send({result,revenue,marred})
     })
-    app.post('/biodata', async (req, res) => {
+
+    app.post('/biodata',verifyToken, async (req, res) => {
       const info = req.body
       const result = await momentBio_Data.insertOne(info)
       res.send(result)
@@ -262,14 +281,15 @@ async function run() {
       const result = await momentBio_Data.find(query).toArray()
       res.send(result)
     })
-    app.get('/biodata/:email', async (req, res) => {
+    app.get('/biodata/:email',verifyToken, async (req, res) => {
       const email = req.params.email
       const query = { contactEmail: email }
+      console.log("biodata 287",email)
       const result = await momentBio_Data.find(query).toArray()
       res.send(result)
     })
 
-    app.put('/biodata/:_id', async (req, res) => {
+    app.put('/biodata/:_id',verifyToken, async (req, res) => {
       const id = req.params._id;
       const userInfo = req.body
         const options = { upsert: true };
@@ -307,7 +327,7 @@ async function run() {
       const result = await momentFav_list.insertOne(info)
       res.send(result)
     })
-    app.get('/fav-list/:email', async (req, res) => {
+    app.get('/fav-list/:email',verifyToken, async (req, res) => {
       const email = req.params.email
       const query = { email }
       const result = await momentFav_list.find(query).toArray()
@@ -322,9 +342,7 @@ async function run() {
     })
 
 
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
   }
   finally {
   }
